f6c7709507c92c1eed36631df3eddb9a4d6e08a6
Tenzin Mahabir <tenzin.mahabir@mail.utoronto.ca>
2020-05-26 01:31:12 -0400

    Added Ability to Receive Input with Multiple Words

--- app.py
+++ app.py
@@ -1,12 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
 
-choice = input("Hello! Would you like to know the current temperature? ")
+choice = input("Hello! Would you like to know the current temperature? ").capitalize()
 
 if (choice == "Yes"):
-    country = input("What Country? ")
-    state = input("Province/State? ")
-    city = input("City? ")
+    country = input("What Country? ").strip()
+    undashedCountry = ""
+    splits = country.split(" ")
+
+    if (len(splits) != 0):
+        country =""
+        for i in range(0, len(splits)):
+            country += splits[i]
+            undashedCountry += splits[i].capitalize()
+            if (i != len(splits)-1):
+                country += "-"
+                undashedCountry += " "
+
+    state = input("Province/State? ").strip()
+    undashedState = ""
+    splits = state.split(" ")
+
+    if (len(splits) != 0):
+        state = ""
+        for i in range(0, len(splits)):
+            state += splits[i]
+            undashedState += splits[i].capitalize()
+            if (i != len(splits) - 1):
+                state += "-"
+                undashedState += " "
+
+    city = input("City? ").strip()
+    undashedCity = ""
+    splits = city.split(" ")
+
+    if (len(splits) != 0):
+        city = ""
+        for i in range(0, len(splits)):
+            city += splits[i]
+            undashedCity += splits[i].capitalize()
+            if (i != len(splits) - 1):
+                city += "-"
+                undashedCity +=" "
 
     url = "https://www.geodatos.net/en/coordinates/"+country+"/"+state+"/"+city
     result = requests.get(url)
@@ -23,8 +58,9 @@ if (choice == "Yes"):
     soup = BeautifulSoup(src, 'lxml')
     mainLinks = soup.find_all('div', attrs={"class": "today_nowcard-section today_nowcard-condition"})
 
+    print(f"The Current Weather Statistics in " + undashedCity +", "+undashedState+", "+undashedCountry+" are...\n")
     temp = mainLinks[0].find('span').text
-    print(f"The temperature right now in "+city+" is "+ temp +" Celcius!")
+    print(f"The temperature right now in "+undashedCity+" is "+ temp +" Celcius!")
     feel = mainLinks[0].find_all('span')[2].text
     print("It feels like", feel)
     high = mainLinks[0].find_all('span')[4].text
@@ -32,7 +68,7 @@ if (choice == "Yes"):
     low = mainLinks[0].find_all('span')[8].text
     print("Today's Low is", low)
     uvIndex = mainLinks[0].find_all('span')[11].text
-    print("Today's UV index is", uvIndex)
+    print("Right now, the UV index is", uvIndex)
 
     sideLinks = soup.find_all('div', attrs={"class": "today_nowcard-sidecar component panel"})
 
