b4213ed1c12439cd53907bddf0d531dc7d2fb48d
Tenzin Mahabir <tenzin.mahabir@mail.utoronto.ca>
2020-05-26 23:53:07 -0400

    Added Ability to Input Coordinates instead of City Name

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ from bs4 import BeautifulSoup
 choice = input("Hello! Would you like to know the current temperature? ").capitalize()
 
 if (choice == "Yes"):
-    country = input("What Country? ").strip()
-    undashedCountry = ""
-    splits = country.split(" ")
 
-    if (len(splits) != 0):
+    decision = input("Would you like to enter the city's name (1) or coordinates (2)? ")
+
+    if (decision == "1"):
+
+        country = input("What Country? ").strip()
+        undashedCountry = ""
+        splits = country.split(" ")
+
         country =""
         for i in range(0, len(splits)):
             country += splits[i]
@@ -17,11 +21,12 @@ if (choice == "Yes"):
                 country += "-"
                 undashedCountry += " "
 
-    state = input("Province/State? ").strip()
-    undashedState = ""
-    splits = state.split(" ")
 
-    if (len(splits) != 0):
+        state = input("Province/State? ").strip()
+        undashedState = ""
+        splits = state.split(" ")
+
+
         state = ""
         for i in range(0, len(splits)):
             state += splits[i]
@@ -29,12 +34,13 @@ if (choice == "Yes"):
             if (i != len(splits) - 1):
                 state += "-"
                 undashedState += " "
+            else:
+                undashedState += ","
 
-    city = input("City? ").strip()
-    undashedCity = ""
-    splits = city.split(" ")
+        city = input("City? ").strip()
+        undashedCity = ""
+        splits = city.split(" ")
 
-    if (len(splits) != 0):
         city = ""
         for i in range(0, len(splits)):
             city += splits[i]
@@ -42,25 +48,40 @@ if (choice == "Yes"):
             if (i != len(splits) - 1):
                 city += "-"
                 undashedCity +=" "
+            else:
+                undashedCity += ","
 
-    url = "https://www.geodatos.net/en/coordinates/"+country+"/"+state+"/"+city
-    result = requests.get(url)
-    src = result.content
-    soup = BeautifulSoup(src, 'lxml')
-    coordLinks = soup.find_all('div', attrs={"class": "col-md-12 fcenter"})
-    coordinates = coordLinks[0].find('strong').text
-    splitWords = coordinates.split()
-    latitude = splitWords[0]
-    longitude = splitWords[1]
 
-    result = requests.get("https://weather.com/en-CA/weather/today/l/"+latitude+longitude)
+        url = "https://www.geodatos.net/en/coordinates/"+country+"/"+state+"/"+city
+
+        result = requests.get(url)
+        src = result.content
+        soup = BeautifulSoup(src, 'lxml')
+        coordLinks = soup.find_all('div', attrs={"class": "col-md-12 fcenter"})
+        coordinates = coordLinks[0].find('strong').text
+        splitWords = coordinates.split()
+        latitude = splitWords[0]
+        longitude = splitWords[1]
+
+        location = undashedCity + " " + undashedState + " " + undashedCountry
+
+    else:
+        latitude = input("What is the latitude of your wanted location? ").strip() + ","
+        longitude = input("What is the longitude of your wanted location? ").strip()
+
+        result = requests.get("https://weather.com/en-CA/weather/today/l/" + latitude + longitude)
+        src = result.content
+        soup = BeautifulSoup(src, 'lxml')
+        location = soup.find('span', attrs={"class": "today_nowcard-loc-title-wrqpper"}).find('h1').text
+
+    result = requests.get("https://weather.com/en-CA/weather/today/l/" + latitude + longitude)
     src = result.content
     soup = BeautifulSoup(src, 'lxml')
     mainLinks = soup.find_all('div', attrs={"class": "today_nowcard-section today_nowcard-condition"})
 
-    print(f"The Current Weather Statistics in " + undashedCity +", "+undashedState+", "+undashedCountry+" are...\n")
+    print(f"\nThe Current Weather Statistics in " + location + " (" + latitude + " " + longitude + ") are...\n")
     temp = mainLinks[0].find('span').text
-    print(f"The temperature right now in "+undashedCity+" is "+ temp +" Celcius!")
+    print(f"The temperature right now is "+ temp +" Celcius!")
     feel = mainLinks[0].find_all('span')[2].text
     print("It feels like", feel)
     high = mainLinks[0].find_all('span')[4].text
@@ -73,8 +94,12 @@ if (choice == "Yes"):
     sideLinks = soup.find_all('div', attrs={"class": "today_nowcard-sidecar component panel"})
 
     wind = sideLinks[0].find_all('span')[0].text
-    direction, strength, unit = wind.split()
-    print(f"Right now, the wind is at a speed of "+ strength + unit+ " in the "+direction+" direction")
+    if (len(wind.split()) == 3):
+        direction, strength, unit = wind.split()
+        print(f"Right now, the wind is at a speed of " + strength + unit + " in the " + direction + " direction")
+    else:
+        print(f"Right now, the wind is " + wind)
+
     humidity = sideLinks[0].find_all('span')[1].text
     print(f"Right now, it is "+humidity+" humidity")
     dewPoint = sideLinks[0].find_all('span')[4].text
